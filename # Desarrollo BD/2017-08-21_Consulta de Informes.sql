84ec63d5424a1e011290acbb4218b32416344f55
sebatista <sebatista@yahoo.com>
2017-08-23 02:53:59 -0300

    Consulta de Informe
    Consulta para presentar datos en informe:
    - Cod_proceso (no visible)
    - Caratula
    - Cod de tipo de proceso (no visible)
    - Tipo de proceso
    - Ultimas novedades
    - Cod_persona (no visible)
    - Nombres, apellidos, razon social, dni, celular (puede faltar alguna)
    del cliente y oponente en caso de existir
    - Cod_condicion (no visible)
    - Condicion
    
    Se entiende que las no visibles si se pueden comentar, genial, sino las
    dejamos en éste archivo pero en php los sacamos, para no marear al
    cliente.

---
+++ # Desarrollo BD/2017-08-21_Consulta de Informes.sql
@@ -0,0 +1,23 @@
+SELECT 	  PRO.cod_proceso, PRO.proceso, PRO.ultimas_novedades
+		, PRO.cod_proceso_tipo, PRO_T.proceso_tipo
+		, PCP_CLI.cod_persona
+        , PER_CLI.nombres, PER_CLI.apellidos, PER_CLI.razon_social, PER_CLI.dni, PER_CLI.celular
+        , PCP_CLI.cod_persona_condicion, COND_CLI.persona_condicion
+        , PCP_OPO.cod_persona
+        , PER_OPO.nombres, PER_OPO.apellidos, PER_OPO.razon_social, PER_OPO.dni, PER_OPO.celular
+        , PCP_OPO.cod_persona_condicion, COND_OPO.persona_condicion
+FROM 	  bsd_proceso AS PRO 
+LEFT JOIN rel_pers_cond_proc AS PCP_CLI 
+	ON (PRO.cod_proceso = PCP_CLI.cod_proceso AND PCP_CLI.cod_persona_condicion = 2)
+LEFT JOIN rel_pers_cond_proc AS PCP_OPO 
+	ON (PRO.cod_proceso = PCP_OPO.cod_proceso AND (PCP_OPO.cod_persona_condicion = 3 OR PCP_OPO.cod_persona_condicion = 4))
+LEFT JOIN bsd_persona AS PER_CLI 
+	ON (PCP_CLI.cod_persona = PER_CLI.cod_persona)
+LEFT JOIN bsd_persona AS PER_OPO
+	ON (PCP_OPO.cod_persona = PER_OPO.cod_persona)
+LEFT JOIN ref_persona_condicion AS COND_CLI
+	ON (PCP_CLI.cod_persona_condicion = COND_CLI.cod_persona_condicion)
+LEFT JOIN ref_persona_condicion AS COND_OPO
+	ON (PCP_OPO.cod_persona_condicion = COND_OPO.cod_persona_condicion)
+		, ref_proceso_tipo AS PRO_T
+WHERE PRO.cod_proceso_tipo = PRO_T.cod_proceso_tipo
